90867ab934bbd62446e0cd9540875321171079b7
AlphaK0307 <keyurpatel1121@gmail.com>
2022-04-06 18:05:26 -0500

    login commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
 
 app = Flask(__name__)
@@ -10,5 +11,10 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate=Migrate(app, db)
 
+login= LoginManager(app)
 
 from app import routes, models
+
+
+
+# 2:03:05 on the lec video
--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
-from enum import unique
-from app import db
+from app import db, login
+from flask_login import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+@login.user_loader
+def get_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username= db.Column (db.String(50), unique= True, nullable=False)
     email= db.Column (db.String(50), unique= True, nullable=False)
@@ -61,4 +65,4 @@ class Phone(db.Model):
         City:{self.city}
         """
 
-    
+  
